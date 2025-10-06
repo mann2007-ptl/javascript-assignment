d2ea7ba0b16087b991aa9124c7e45467a1a57791
mann2007-ptl <mann.patel.cg@gmail.com>
2025-10-06 18:26:41 +0530

    javascript assignments

--- assign/q2.js
+++ assign/q2.js
@@ -3,13 +3,14 @@ var units=400;
 var bill=0;
 
 if(units<=100){
-    bill = units * 5;
+    bill=units*5;
+    console.log("the bill payed is:" + bill);
 }
 else if(units<=300){
-    bill = (100 * 5) + ((units - 100) * 7);
+    bill=(100*5)+((units-100)*7);
+    console.log("the bill payed is:" + bill);
 }
 else{
-    bill=(100 * 5) + (200 * 7) + ((units - 300) * 10);
+    bill=(100*5)+(200*7)+((units-300)*10);
+    console.log("the bill payed is:" + bill);
 }
-
-console.log("the bill payed is:" + bill);
--- assign/q3.js
+++ assign/q3.js
@@ -3,16 +3,18 @@ var income=750000;
 var tax=0;
 
 if(income<=250000){
-    tax =0;
+    tax=0;
+    console.log("tax payed is :" + tax);
 }
 else if(income<=500000){
     tax=(income-250000) * 0.05;
+    console.log("tax payed is :" + tax);
 }
 else if(income<=1000000){
     tax=(500000-250000) * 0.05 + (income-500000) * 0.20;
+    console.log("tax payed is :" + tax);
 }
 else{
     tax=(500000-250000) * 0.05 + (1000000-500000) * 0.20 + (income-1000000) * 0.30;
+    console.log("tax payed is :" + tax);
 }
-
-console.log("income tax payed is :" + tax);
